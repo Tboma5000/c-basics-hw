a616f5196fe99b27f00a31522809e07c577ee805
Tboma5000 <vorobjovartem300@gmail.com>
2025-08-20 13:37:47 +0300

    fully done

--- Exercise2/cod.c
+++ Exercise2/cod.c
@@ -2,6 +2,7 @@
 
 int main() {
 	char  letter1, letter2, letter3;
+	int moving;
 
 	printf("Enter 3 letter that you want to encrypt \n");
 	printf("Enter your first letter: \n");
@@ -10,12 +11,14 @@ int main() {
 	scanf(" %c", &letter2);
 	printf("Enter your third letter: \n");
 	scanf(" %c", &letter3);
+	printf("Enter the number to be moved: \n");
+	scanf(" %d", &moving);
 
 	printf("Your input is: %c %c %c \n", letter1, letter2, letter3);
 	
-	letter1+=1;
-	letter2+=1;
-	letter3+=1;
+	letter1+=moving;
+	letter2+=moving;
+	letter3+=moving;
 	printf("Your message now: %c %c %c \n", letter1, letter2, letter3);
 	return 0;
 }
