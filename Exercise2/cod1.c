08b9ec0dfad9683a28b0f62062b35bd370b7185e
Tboma5000 <vorobjovartem300@gmail.com>
2025-08-20 21:36:39 +0300

    encryptor

--- Exercise2/cod1.c
+++ Exercise2/cod1.c
@@ -3,11 +3,19 @@
 
 int main() {
 	char word[20];
-	int moving;
+	
 
 	printf("Enter word that you want to encrypt: \n");
-	scanf("%s", word);
-	
-	printf("Your word: %s \n", word);
+	fgets(word, sizeof(word), stdin);
+
+	size_t len = strlen(word)-1;
+
+		//printf("Lenght: %ld \n", len);
+
+	for (int i=0; i<len; i+=1) {
+		word[i]+=1;
+		//printf("%s \n", word);
+	}
+	printf("Your encrypted word: %s \n", word);
 	return 0;
 }
