3433cc0d2cce664ad2fef7cbf57a122edcfed684
Tboma5000 <vorobjovartem300@gmail.com>
2025-08-20 13:22:40 +0300

    First block

---
+++ Exercise2/cod.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	char letter1, letter2, letter3;
+
+	printf("Enter 3 letter that you want to encrypt \n");
+	printf("Enter your first letter: \n");
+	scanf(" %c", &letter1);
+	printf("Enter your second letter: \n");
+	scanf(" %c", &letter2);
+	printf("Enter your third letter: \n");
+	scanf(" %c", &letter3);
+
+	printf("Your input is: %c %c %c \n", letter1, letter2, letter3);
+	return 0;
+}
