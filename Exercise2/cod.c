13e3d35956358115800dafc68ce05071f4058691
Tboma5000 <vorobjovartem300@gmail.com>
2025-08-20 13:29:43 +0300

    done

--- Exercise2/cod.c
+++ Exercise2/cod.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	char letter1, letter2, letter3;
+	char  letter1, letter2, letter3;
 
 	printf("Enter 3 letter that you want to encrypt \n");
 	printf("Enter your first letter: \n");
@@ -12,5 +12,10 @@ int main() {
 	scanf(" %c", &letter3);
 
 	printf("Your input is: %c %c %c \n", letter1, letter2, letter3);
+	
+	letter1+=1;
+	letter2+=1;
+	letter3+=1;
+	printf("Your message now: %c %c %c \n", letter1, letter2, letter3);
 	return 0;
 }
