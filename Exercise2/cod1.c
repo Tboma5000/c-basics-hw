622a9c594edcc1a7eadea6de522c80513384d47d
Tboma5000 <vorobjovartem300@gmail.com>
2025-08-20 14:41:01 +0300

    Enter

---
+++ Exercise2/cod1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char word[20];
+	int moving;
+
+	printf("Enter word that you want to encrypt: \n");
+	scanf("%s", word);
+	
+	printf("Your word: %s \n", word);
+	return 0;
+}
