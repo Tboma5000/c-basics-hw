8e263229cb59c1600ef30d5544597b507954e76e
Tboma5000 <vorobjovartem300@gmail.com>
2025-08-30 03:56:19 +0300

    Exercise3 done

--- hm-2/Exercise3*/cod.c
+++ hm-2/Exercise3*/cod.c
@@ -1,34 +1,29 @@
 #include <stdio.h>
 
 int main() {
-	int numb = 3;
-	//printf("Enter a number: \n");
-	//scanf(" %d", &numb);
-/*
+	int numb;
+	int max;
+	printf("Enter a number: \n");
+	scanf(" %d", &numb);
+	
 	int comparing = numb % 2;
+	int progression = (numb / 2);
+	
 	if (comparing == 0) {
-		max = numb + 1;
+		max = numb + progression + progression-1;
 	}
 	else {
-		max = numb + 2;
+		max = numb + progression + progression;
+		//printf(" %d \n", max);
 	}
-*/	
-	printf(" %d", numb);
-	int max = numb + 2;
-	printf(" %d", max);
-	int matrix[max-1][max-1];
-
-	for (int i=0; i < max ; i++) {
-		for (int j=0; j < max; j++) {
-			matrix[i][j] = numb;
-			//printf(" %d", matrix[i][j]);
-		}	
-		//printf("\n");
-	}
-
-	for (int i=1; i < max-1; i++) {
-		for (int j=1; j < max-1; j++) {
-			matrix[i][j] = numb - 1;
+	int matrix[max][max];
+	for (int move=0; move < max; move++) {
+		for (int i=0+move; i < max-move ; i++) {
+			for (int j=0+move; j < max-move; j++) {
+				matrix[i][j] = numb-move;
+				//printf(" %d", matrix[i][j]);
+			}	
+			//printf("\n");
 		}
 	}
 
