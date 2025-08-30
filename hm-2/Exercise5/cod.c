5999f348de576efdb44e43ab612f88ff52ed7601
Tboma5000 <vorobjovartem300@gmail.com>
2025-08-30 20:54:06 +0300

    Exercise5 done

--- hm-2/Exercise5/cod.c
+++ hm-2/Exercise5/cod.c
@@ -5,11 +5,13 @@ int main() {
 	printf("Enter min and max value: ");
 	scanf(" %d %d", &min, &max);
 
-	for (int i=1; i<=max-min+1 ; i++) {
+	for (int i=1; i<=max-min-1; i++) {
 		printf("%d ", min+i);
 	}
 	printf("\n");
-	for (int i=2; i<=max-min+1; i+=2) {
+
+	//This part had created, when I don`t understand task and do something similar:
+	for (int i=2; i<=max-min; i+=2) {
 		printf("%d ", min+i);
 	}
 	return 0;
