9b4d39b56fb21760dcd7383be693b269c5e3ff2e
Tboma5000 <vorobjovartem300@gmail.com>
2025-09-07 13:53:44 +0200

    Multiplication in progress

--- hm-3/Exercise2/cod.c
+++ hm-3/Exercise2/cod.c
@@ -82,12 +82,28 @@ int main() {
 
                 }
                 else {
-                        printf("\nYou can`t add matrixs with different quantity of rows and columns");
+                        printf("\nYou can`t subtract matrixs with different quantity of rows and columns");
                 }
 	}
+	/* In progress
 	else if (operator=='*') {
-
+	 	if (y1==x2) {
+			int matrixResult[x1][y2];
+			//matrixResult[0][0] = matrix1[0][0] * matrix2[0][0] + matrix1[0][1] * matrix2[1][0];
+			//matrixResult[0][1] = matrix1[0][0] * matrix2[0][1] + matrix1[0][1] * matrix2[1][1];
+			//matrixResult[1][0] = matrix1[1][0] * matrix2[0][1] + matrix1[1][1] * matrix2[1][1];
+			//matrixResult[1][1] = matrix1[1][0] * matrix2[1][0] + matrix1[1][1] * matrix2[2][0];
+			for (int i=0; ; i++) {
+				for (int j=0; ; j++) {
+					
+				}
+			}
+		}
+		else {
+			printf("\nError. You can`t multiply matrixs, where quantity of rows and columns are different");
+		}	
 	}
+	*/
 	else {
 		printf("\nError");
 	}
